cddaf31a3221ec5f4736ac56f21a19fcd938bab3
ShunaulaJ <josepher.shunaula@gmail.com>
2023-03-13 00:42:25 -0400

    Task 2 Completed

--- RideSharer/RideHistoryViewController.swift
+++ RideSharer/RideHistoryViewController.swift
@@ -6,22 +6,11 @@
 
 import UIKit
 
-class RideHistoryViewController: UIViewController, UITableViewDataSource {
+class RideHistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
-    
+
     let rideHistory = [("Driver: Joe, 12/29/2021", "$26.50"), ("Driver: Sandra, 01/03/2022", "$13.10"), ("Driver: Hank, 01/11/2022", "$16.20"), ("Driver: Michelle, 01/19/2022", "$8.50")]
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rideHistory.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
-        let itemInfo = rideHistory[indexPath.row]
-        cell.textLabel?.text = "\(itemInfo.0), \(itemInfo.1)"
-        return cell
-    }
     
     
     override func viewDidLoad() {
@@ -29,6 +18,28 @@ class RideHistoryViewController: UIViewController, UITableViewDataSource {
         title = "History"
         
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "tableCell")
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rideHistory.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let itemInfo = rideHistory[indexPath.row]
+        cell.textLabel?.text = "\(itemInfo.0)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Price", message: "\(rideHistory[indexPath.row].1)", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
